fd9d07244af86e011eda8d7cc2a5b489be0f4d0c
jaewoopk <jaewoopk000@naver.com>
2022-09-12 16:57:33 +0900

    Day 3 Finally solve with assignment1

--- 19012067_박재우.py
+++ 19012067_박재우.py
@@ -1,6 +1,6 @@
 import re
 
-f = open("Input01.txt", "r")
+f = open("Input02.txt", "r")
 
 data = f.readlines()
 
@@ -13,5 +13,24 @@ for i in range((int)(data[(int)(data[1][2]) + 3][2])) :
     one_line = re.findall("-?\d+", data[i + (int)(data[1][2]) + 4])
     list.append(one_line)
 
-print(list)
-f.close
+file = open("Output02.txt", "w")
+
+if (data[1][2] != data[(int)(data[1][2]) + 3][2] and\
+    data[1][4] != data[(int)(data[1][2]) + 3][4]) :
+    file.writelines("+ 계산 불가")
+
+else :
+    answer = []
+    for i in range((int)(data[1][2])) :
+        one_answer = []
+        for j in range((int)(data[1][4])) :
+            one_answer.append((int)(list[i][j]) + (int)(list[i + (int)(data[1][2])][j]))
+        answer.append(one_answer)
+        answer.append('\n')
+    
+    for lines in answer :
+        result = ' '.join(str(i) for i in lines)
+        file.writelines(result)
+
+file.close()
+f.close()
