86c85c97e3b38c420936389d4bf9d33291eb1277
jaewoopk <jaewoopk000@naver.com>
2022-10-06 14:02:28 +0900

    Day 5 I think solve it about assignment linear algebra

--- 19012067_박재우2.py
+++ 19012067_박재우2.py
@@ -1,13 +1,25 @@
 import re
 
-def determinant(arr, x) :
-    if (x == 0) :
-        return arr[0][x] * (arr[1][1] * arr[2][2] - arr[1][2] * arr[2][1])
-    elif (x == 1) :
-        return arr[0][x] * (arr[1][0] * arr[2][2] - arr[1][2] * arr[2][0])
-    elif (x == 2) :
-        return arr[0][x] * (arr[1][0] * arr[2][1] - arr[1][1] * arr[2][0])
+det = 0
 
+def determinant(arr, x) :
+    global det
+    if (x == 3) :
+        return arr[0][0] * (arr[1][1] * arr[2][2] - arr[1][2] * arr[2][1]) - (arr[0][1] * (arr[1][0] * arr[2][2] - arr[1][2] * arr[2][0])) + (arr[0][2] * (arr[1][0] * arr[2][1] - arr[1][1] * arr[2][0]))
+    else :
+        for i in range (x) :
+            mini_arr = []
+            for j in range(1, x) :
+                row = []
+                for k in range(x) :
+                    if (i == k) : continue
+                    row.append(arr[j][k])
+                mini_arr.append(row)
+            print(mini_arr)
+            if (i % 2 == 0) :
+                det += (arr[0][i] * determinant(mini_arr, x - 1))
+            else :
+                det += (-1 * arr[0][i] * determinant(mini_arr, x - 1))
 
 f = open("Input.txt", "r")
 
@@ -28,13 +40,21 @@ if (count != length) :
     print("Square Matrix가 성립하지 않아, determinant를 구할 수 없습니다.")
     exit()
 
-for i in range(count) :
-    if (i % 2 == 0) :
-        ans += determinant(list, i)
-    else :
-        ans += (-1 * determinant(list, i))
-        
-print(ans)
+if (count == 3) :
+    det = determinant(list, count)
+else :
+    determinant(list, count)
+
+print(det)
+
+# 0 0 2 0
+# 1 2 17 -3
+# -1 0 28 1
+# -2 2 -37 1
+
+# 2 -4 3
+# 3 1 2
+# 1 4 -1
 
 #for i in range((int)(data[1][2])) :
 #    one_line = re.findall("-?\d+", data[i + 2])
