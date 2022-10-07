00556e7a54db250e078bbe88f5738020a398ae9d
jaewoopk <jaewoopk000@naver.com>
2022-10-07 22:38:15 +0900

    Day 6 All done for assignment 2

--- 19012067_박재우2.py
+++ 19012067_박재우2.py
@@ -2,24 +2,24 @@ import re
 
 def determinant(arr, x) :
     answer = 0
-    if (x == 3) :
+    if (x == 3) : # 3x3 정사각형 행렬일 때, determinant값을 재귀 함수의 return값으로 설정합니다.
         answer = arr[0][0] * (arr[1][1] * arr[2][2] - arr[1][2] * arr[2][1]) - (arr[0][1] * (arr[1][0] * arr[2][2] - arr[1][2] * arr[2][0])) + (arr[0][2] * (arr[1][0] * arr[2][1] - arr[1][1] * arr[2][0]))
         return answer
-    else :
+    else : # 3x3 정사각형 행렬이 아닐 때,
         for i in range (x) :
             mini_arr = []
             for j in range(1, x) :
                 row = []
                 for k in range(x) :
                     if (i == k) : continue
-                    row.append(arr[j][k])
-                mini_arr.append(row)
-            det = determinant(mini_arr, x - 1)
-            if (i % 2 == 0) :
+                    row.append(arr[j][k]) # cofactor algorithm의 원리를 이용하여 row를 저장하고
+                mini_arr.append(row) # 그 행렬을 mini_arr에 저장합니다.
+            det = determinant(mini_arr, x - 1) # 변수 det에 determinant 재귀함수를 통해 값을 구합니다.
+            if (i % 2 == 0) : # 행렬의 1번째 row의 index가 짝수면 그대로,
                 answer += (arr[0][i] * det)
-            elif (i % 2 == 1) :
+            else : # 행렬의 1번째 row의 index가 홀수면 -1을 곱해 answer에 저장합니다.
                 answer += (-1 * arr[0][i] * det)
-    return answer
+    return answer # 변수 answer를 리턴합니다.
 
 f = open("Input.txt", "r")
 
@@ -29,74 +29,21 @@ i = 0
 
 file = open("Output.txt", "w")
 
-for _ in range(3) :
+for _ in range(3) : # 변수 data에 저장된 Input.txt파일안에서 개행문자를 통해 분리하여 행렬을 찾아냅니다.
     list = []
     while (i < length) :
         one_line_str = re.findall("-?\d+", data[i])
         i += 1
         one_line = [int(val) for val in one_line_str]
-        if not one_line :
+        if not one_line : # 개행문자일 경우 while문을 break하여 만들어진 행렬을 통해 determinant값을 구합니다.
             break
         list.append(one_line)
 
-    if (len(list) != len(list[0])) :
+    if (len(list) != len(list[0])) : # 만들어진 행렬의 row값과 column값을 비교하여 정사각행렬이 아닐경우 det를 구할 수 없습니다.
         file.writelines("Square Matrix가 성립하지 않아, determinant를 구할 수 없습니다.\n")
     else :
         det = determinant(list, len(list))
         file.writelines((str)(det) + "\n")
-
-
-#count = len(one_line)
-
-#if (count != length) :
-#    print("Square Matrix가 성립하지 않아, determinant를 구할 수 없습니다.")
-#    exit()
-
-# real_answer = determinant(list, count)
-
-# print(real_answer)
-
-# 0 0 2 0
-# 1 2 17 -3
-# -1 0 28 1
-# -2 2 -37 1
-
-# 2 -4 3
-# 3 1 2
-# 1 4 -1
-
-# 1 1 1 2 1
-# 1 2 2 1 1
-# 1 3 3 0 1
-# 1 2 2 2 2
-# 1 1 1 1 1
-
-#for i in range((int)(data[1][2])) :
-#    one_line = re.findall("-?\d+", data[i + 2])
-#    list.append(one_line)
-
-#for i in range((int)(data[(int)(data[1][2]) + 3][2])) :
-#    one_line = re.findall("-?\d+", data[i + (int)(data[1][2]) + 4])
-#    list.append(one_line)
-
-#file = open("Output.txt", "w")
-
-#if (data[1][2] != data[(int)(data[1][2]) + 3][2] or\
-#    data[1][4] != data[(int)(data[1][2]) + 3][4]) :
-#    file.writelines("+ 계산 불가")
-
-#else :
-#    answer = []
-#    for i in range((int)(data[1][2])) :
-#        one_answer = []
-#        for j in range((int)(data[1][4])) :
-##            one_answer.append((int)(list[i][j]) + (int)(list[i + (int)(data[1][2])][j]))
-#        answer.append(one_answer)
-#        answer.append('\n')
-    
-#    for lines in answer :
-#        result = ' '.join(str(i) for i in lines)
-#        file.writelines(result)
 
 file.close()
 f.close()
